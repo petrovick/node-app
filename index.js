b3f4a96d9ca1832f276c1cf2430db29325cba278
petrovick <petrovick@users.noreply.github.com>
2015-06-04 00:43:06 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+var CCDA = require("./lib/CCDA/index.js");
+
+//CCDA metadata stuff
+var meta = {};
+meta.CCDA = CCDA;
+
+meta.supported_sections = [
+    'allergies',
+    'procedures',
+    'immunizations',
+    'medications',
+    'encounters',
+    'vitals',
+    'results',
+    'social_history',
+    'demographics',
+    'problems',
+    'insurance',
+    'claims',
+    'plan_of_care',
+    'payers',
+    'providers',
+    'organizations'
+];
+
+meta.code_systems = require("./lib/code-systems");
+
+module.exports = exports = meta;
